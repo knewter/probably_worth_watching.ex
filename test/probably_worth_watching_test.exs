dc8f565107e71b788012f65b78fc60acbb5188c4
Josh Adams <josh.rubyist@gmail.com>
2015-12-16 05:06:37 -0600

    Build out the example network as Nodes

--- test/probably_worth_watching_test.exs
+++ test/probably_worth_watching_test.exs
@@ -25,4 +25,36 @@ defmodule ProbablyWorthWatchingTest do
     assert %Node{} = elixir_tweets
     assert length(elixir_tweets.sinks) == 2
   end
+
+  test "the network" do
+    count_7d_elixir = Node.new
+    count_7d_ruby = Node.new
+    count_7d_javascript = Node.new
+    count_7d_has_link = Node.new
+    count_7d_has_video = Node.new
+    count_7d_decorate_video = Node.new
+
+    decorate_video = Node.new
+    decorate_video = Node.add_sink(decorate_video, count_7d_decorate_video)
+
+    has_video = Node.new
+    has_video = Node.add_sink(has_video, decorate_video)
+    has_video = Node.add_sink(has_video, count_7d_has_video)
+
+    has_link = Node.new
+    has_link = Node.add_sink(has_link, has_video)
+    has_link = Node.add_sink(has_link, count_7d_has_link)
+
+    tweets_elixir = Node.new
+    tweets_elixir = Node.add_sink(tweets_elixir, has_link)
+    tweets_elixir = Node.add_sink(tweets_elixir, count_7d_elixir)
+
+    tweets_ruby = Node.new
+    tweets_ruby = Node.add_sink(tweets_ruby, has_link)
+    tweets_ruby = Node.add_sink(tweets_ruby, count_7d_ruby)
+
+    tweets_javascript = Node.new
+    tweets_javascript = Node.add_sink(tweets_javascript, has_link)
+    tweets_javascript = Node.add_sink(tweets_javascript, count_7d_javascript)
+  end
 end
